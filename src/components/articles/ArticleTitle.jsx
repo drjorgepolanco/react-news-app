e72c4261daac8c1825bc07d44b97eb567a5ad2cc
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2016-05-25 16:17:30 -0400

    Create component ArticleTitle

---
+++ src/components/articles/ArticleTitle.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var ArticleTitle =  React.createClass({
+    render: function() {
+        return (<h3>{this.props.titleCopy}</h3>);
+    }
+});
+
+module.exports = ArticleTitle;
