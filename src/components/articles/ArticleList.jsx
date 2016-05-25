d8b4503a9dad131b6f57e826648fbe67e1607188
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2016-05-25 18:34:23 -0400

    Create component ArticleList to iterate through Articles

---
+++ src/components/articles/ArticleList.jsx
@@ -0,0 +1,52 @@
+var React   = require('react');
+var Article = require('./Article.jsx');
+
+var idCounter = 0;
+
+var articles = [
+    {
+        "id": idCounter++,
+        "title": "First Article Title",
+        "subtitle": "First Article Subtitle",
+        "content": "Nonne vides quid sit? Tu es ... Jesse me respice. Tu ... blowfish sunt. A blowfish! Cogitare. Statura pusillus, nec sapientium panem, nec artificum. Sed predators facile prædam blowfish secretum telum non se habet. Non ille? Quid faciam blowfish, Isai. Blowfish quid faciat? In blowfish inflat, purus?",
+        "thumb": "http://i.imgur.com/hOtWXAp.gif",
+        "location": "New York, NY"
+    },
+    {
+        "id": idCounter++,
+        "title": "Second Article Title",
+        "subtitle": "Second Article Subtitle",
+        "content": "Blowfish librantur in se quatuor, quinquies maior quam normalis, et quare? Quare id faciam? Ut terrorem facit, qui quid. Terrent! Ut alter, scarier pisces agminis off. Et quod tu es? Vos blowfish. Tu iustus in omnibus visio. Vides ... suus ' suus 'non aliud aerem. Nunc ... qui cum partibus blowfish Isai? Tu damnare ius. Vos blowfish. Dicere iterum. Dicere illam quasi velis eam. Es BLOWFISH!",
+        "thumb": "http://i.imgur.com/NdOut3L.gif",
+        "location": "Santa Monica, CA"
+    },
+    {
+        "id": idCounter++,
+        "title": "Third Article Title",
+        "subtitle": "Third Article Subtitle",
+        "content": "Ut sibi fuerat socius sagittis. Ego intervenerit. Vere quia a te nuper iratus occidit illos undecim annorum puer. Deinde, si hoc forte qui fuit imperavit.",
+        "thumb": "http://i.imgur.com/gIo45d8.gif",
+        "location": "Miami, FL"
+    },
+    {
+        "id": idCounter++,
+        "title": "Fourth Article Title",
+        "subtitle": "Fourth Article Subtitle",
+        "content": "Quod satis pecuniae sempiternum. Ut sciat oportet motum. Nunquam invenies eum. Hic de tabula. Ego vivere, ut debui, et nunc fiant. Istuc quod opus non est. Lorem ipsum occurrebat pragmaticam semper ut, si quis ita velim tibi bene recognoscere. Quorum duo te mihi videtur.",
+        "thumb": "http://i.imgur.com/TFM6t7A.gif",
+        "location": "Minneapolis, MN"
+    }
+];
+
+var ArticleList = React.createClass({
+    render: function() {
+        
+        var listOfArticles = articles.map(function(article) {
+            return (<Article key={article.id} title={article.title} subtitle={article.subtitle} content={article.content} thumb={article.thumb} location={article.location} />);
+        });
+
+        return (<ul>{listOfArticles}</ul>);
+    }
+});
+
+module.exports = ArticleList;
