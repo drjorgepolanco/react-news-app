449703b4a9d3d93fc0b01dcca28b90e9a7c13a45
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2016-05-25 18:40:24 -0400

    Create src/main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+var React       = require('react');
+var ReactDOM    = require('react-dom');
+var ArticleList = require('./components/articles/ArticleList.jsx');
+
+ReactDOM.render(<ArticleList />, document.getElementById('article-list'));
