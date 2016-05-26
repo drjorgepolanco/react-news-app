d13cc47a0da1a8f9627c565868ff9622c3af1460
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2016-05-26 14:45:37 -0400

    Create component ArticleUser to render the user profile pic inside the Article

---
+++ src/components/articles/ArticleUser.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var ArticleUser = React.createClass({
+    render: function() {
+        return (
+            <img src={this.props.articleUserShot} />
+        );
+    }
+});
