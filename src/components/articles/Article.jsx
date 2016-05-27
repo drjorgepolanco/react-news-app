e2430625fdff4c919c3070f4562cd3a582ba7f8f
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2016-05-27 10:20:20 -0400

    Add markup to Article.jsx to include the profile pic

--- src/components/articles/Article.jsx
+++ src/components/articles/Article.jsx
@@ -4,23 +4,30 @@ var ArticleSubtitle = require('./ArticleSubtitle.jsx');
 var ArticleContent  = require('./ArticleContent.jsx');
 var ArticleThumb    = require('./ArticleThumb.jsx');
 var ArticleLocation = require('./ArticleLocation.jsx');
+var ArticleUser     = require('./ArticleUser.jsx');
 
 var Article = React.createClass({
     render: function() {
-        var title    = this.props.title,
-            subtitle = this.props.subtitle,
-            content  = this.props.content,
-            thumb    = this.props.thumb,
-            location = this.props.location;
+        var title      = this.props.title,
+            subtitle   = this.props.subtitle,
+            content    = this.props.content,
+            thumb      = this.props.thumb,
+            location   = this.props.location;
+            profilePic = this.props.profilePic;
 
         return (
-            <li className="row">
-                <div className="col-xs-12">
-                    <ArticleTitle articleTitleText={title} />
-                    <ArticleSubtitle articleSubtitleText={subtitle} />
-                    <ArticleContent articleContentText={content} />
-                    <ArticleThumb articleThumbSrc={thumb} />
-                    <ArticleLocation articleLocationText={location} />
+            <li className="row panel panel-default">
+                <div className="col-xs-12 inner">
+                    <div className="col-xs-12 col-sm-2">
+                        <ArticleUser articleUserProfilePic={profilePic} />
+                    </div>
+                    <div className="col-xs-12 col-sm-10">
+                        <ArticleTitle articleTitleText={title} />
+                        <ArticleSubtitle articleSubtitleText={subtitle} />
+                        <ArticleContent articleContentText={content} />
+                        <ArticleThumb articleThumbSrc={thumb} />
+                        <ArticleLocation articleLocationText={location} />
+                    </div>
                 </div>
             </li>
         );
