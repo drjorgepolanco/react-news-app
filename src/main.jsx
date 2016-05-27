96c9f247fcb68cb4b7c201b42e4815bc1eeb3815
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2016-05-27 10:46:05 -0400

    Require SocialIconsList from main.jsx and render it

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
-var React    = require('react');
-var ReactDOM = require('react-dom');
-var List     = require('./components/List.jsx');
+var React           = require('react');
+var ReactDOM        = require('react-dom');
+var ArticleList     = require('./components/articles/ArticleList.jsx');
+var SocialIconsList = require('./components/social/SocialIconsList.jsx');
 
-ReactDOM.render(<List />, document.getElementById('article-list'));
+ReactDOM.render(<ArticleList />, document.getElementById('article-list'));
+ReactDOM.render(<SocialIconsList />, document.getElementById('social-icons-list'));
